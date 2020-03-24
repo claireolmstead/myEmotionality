bfe7b05195f6409c6915ab0b1a360b871d4504dc
Virginia Claire Olmstead <43625033+viol5229@users.noreply.github.com>
2020-03-24 16:18:30 -0700

    Delete index.js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$("#navDopdown").click(function(){
-	$("nav").toggle("slow");
-});
-
-$(".choiceButton").click(function() {
-	var title = this.id;
-	console.log("hello");
-	$("playlistTitle").html("hello");
-});
